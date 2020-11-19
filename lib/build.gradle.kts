dace2fe1eaf6ce1d16f585ff7d2eba71af66f32b
Antonio Zugaldia <antonio@mapbox.com>
2020-11-19 17:05:38 -0500

    bump dependencies - jackson-module-kotlin:2.12.0-rc2 does bring 1.4.x.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,7 +14,7 @@ tasks.withType<ProcessResources> {
 
 dependencies {
     // Log4j2 (API only)
-    implementation("org.apache.logging.log4j:log4j-api:2.13.3")
+    implementation("org.apache.logging.log4j:log4j-api:2.14.0")
 
     // Testing
     testImplementation("com.squareup.okhttp3:mockwebserver:3.14.9")
