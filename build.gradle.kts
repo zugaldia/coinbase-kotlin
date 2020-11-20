c823dd743325fddb3e06090bc6892468ce31a6f9
Antonio Zugaldia <antonio@mapbox.com>
2020-11-19 21:34:33 -0500

    Update to Kotlin v1.4 (#6)
    * replace SLF4J with Log4j2
    
    * update to latest jackson (and hence kotlin 1.4)
    
    * bump dependencies - jackson-module-kotlin:2.12.0-rc2 does bring 1.4.x.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 
     // Matches the version in jackson-module-kotlin to avoid warnings
     // around unmatching versions of Kotlin reflection
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.10"
 
     // Determines which dependencies have updates
-    id("com.github.ben-manes.versions") version "0.33.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 allprojects {
@@ -29,18 +29,18 @@ subprojects {
         // Retrofit
         implementation("com.squareup.retrofit2:retrofit:2.9.0")
         implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0-rc2")
 
         // Public API for the Coinbase client
         // TODO: do we need to leak Joda Time as part of the public API?
         api("com.squareup.okhttp3:okhttp:3.14.9")
-        api("joda-time:joda-time:2.10.6")
+        api("joda-time:joda-time:2.10.8")
 
         // Request signing
         implementation("commons-codec:commons-codec:1.15")
 
         // Testing
-        testImplementation("junit:junit:4.13")
+        testImplementation("junit:junit:4.13.1")
     }
 
     configure<JavaPluginConvention> {
